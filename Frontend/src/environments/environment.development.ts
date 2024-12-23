c1b13e94761030bd42213fb489a38069f53db1e1
Adem Bahri <103949052+Bahri-Adem@users.noreply.github.com>
2024-12-23 15:02:45 +0100

    Last Commit

---
+++ Frontend/src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  keycloak: {
+    url: "http://localhost:9080",
+    realm: "WhatsApp-Clone",
+    clientId: "whatsapp-clone"
+  },
+  API_URL: "http://localhost:4200/api",
+};
